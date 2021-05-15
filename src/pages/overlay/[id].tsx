2198545e5d6dd1e511d1fe62149fc8a351725d48
Miguel <miguel.sanseverino@gmail.com>
2021-05-15 12:07:38 -0300

    overlay transparent body

--- src/pages/overlay/[id].tsx
+++ src/pages/overlay/[id].tsx
@@ -2,17 +2,24 @@ import { Box, Flex, Stack } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Overlay from "../../components/overlay";
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: transparent;
+  }
+`
 
 
 const OverlayPage: React.FC<any> = ({props}) => {
+
   const router = useRouter();
   const pid: number = Number(router.query.id as string);
-  const [count, setCount] = useState(0);
-  const [playerData, setPlayerData] = useState()
 
+  
 	return (
     <Box backgroundColor="transparent">
+      <GlobalStyle/>
       <Overlay id={pid as number}/>
     </Box>
 	)
